396e45d499f4f9e6e4e2120c4a2073376b682277
mottak <kissylamottapereira@gmail.com>
2023-10-19 07:56:12 -0300

    chore: cria validação de novo usuário

---
+++ src/main/validators/userValidator.ts
@@ -0,0 +1,12 @@
+import { NewUser } from '../../domain/models'
+import Joi from 'joi'
+
+
+export const userSchema = Joi.object<NewUser>({
+  name: Joi.string()
+    .min(3)
+    .required(),
+  email: Joi.string()
+    .email()
+    .required()
+})
