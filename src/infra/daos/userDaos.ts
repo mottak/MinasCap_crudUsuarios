f8d68f0435cf5b2cf13bf7dba10445e640bb21d7
mottak <kissylamottapereira@gmail.com>
2023-10-19 15:11:47 -0300

    chore: cria daos para tabela de usuários

--- src/infra/daos/userDaos.ts
+++ src/infra/daos/userDaos.ts
@@ -5,8 +5,15 @@ import UserModel from "../sequelize/models/User";
 
 export class UserDAO implements IUserRepo {
   async add(data: NewUser): Promise<User> {
-    const user = await UserModel.create(data);
-    return user
+     
+    const user = await UserModel.create({ name: data.name, email: data.email });
+
+    return user;
+  }
+
+  async findByEmail(data: NewUser['email']): Promise<User> {
+    const user = await UserModel.findOne({ where: { email: data }});
+    return user as unknown as User;
   }
   
 
